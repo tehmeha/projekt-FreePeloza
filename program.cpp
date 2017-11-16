ab6165d0c550a25456dd8672729affdcc7512cd9
FreePeloza <stjepannovoselic1@gmail.com>
2017-11-16 09:54:55 +0100

    Rezultati

--- program.cpp
+++ program.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main()
 {
+    int mi[20],vi[20];
+
+    cout<<"1. Upiste bodove za ekipu mi"<<endl;
+    cout<<"2. Upiste bodove za ekipu vi"<<endl;
+    cout << "Unesite 1. ili 2."<<endl;
+    int izbor;
+    cin>>izbor;
+    int brIgre = 0;
+    if( izbror==1)
+    {
+        cout << "Upisite bodove za ekipu mi"<<endl;
+        cin >> mi[brIgre];
+        vi[brIgre] = 162 - mi[brIgre];
+    }
+    else if( izbror==2)
+    {
+        cout << "Upisite bodove za ekipu vi"<<endl;
+        cin >> vi[brIgre];
+        mi[brIgre] = 162 - vi[brIgre];
+    }
     return 0;
 }
